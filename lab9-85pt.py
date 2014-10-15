2cf5521699df4038f29799987787e7dbf6f8635d
mactheknight <s650551@student.roundrockisd.org>
2014-10-15 15:47:37 -0500

    first

--- lab9-85pt.py
+++ lab9-85pt.py
@@ -10,7 +10,9 @@
 myList = [102,98,96,101,100,99,103,97,98,105]
 
 # Insert for loop here
-
+for x in myList(100):
+    if myList >= 100:
+        
 
 # This should print [102,101,103,105]
 print myList
